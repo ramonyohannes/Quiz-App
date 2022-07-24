2ea896c60a93160810ba923bb1999c68bfddf111
ramon <ramonebeyan@gmail.com>
2022-07-24 21:24:45 +0300

    Added Expanded Widgets

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -7,10 +7,38 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Quiz App"),
+        title: const Text(
+          "Quiz App",
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        elevation: 0,
+        centerTitle: true,
+        backgroundColor: Colors.white10,
       ),
-      body: Container(
-        color: Colors.grey,
+      body: Column(
+        children: [
+          Expanded(
+            flex: 2,
+            child: Container(
+              color: Colors.amber,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.red,
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              color: Colors.green,
+            ),
+          ),
+        ],
       ),
     );
   }
